c291a2f5395168c1f7c872b2ab5c9ec555340b44
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-01-16 01:33:03 -0300

    feat(pages/SignIn): added react-hook-form and zod integrations

--- src/pages/auth/SignIn/index.tsx
+++ src/pages/auth/SignIn/index.tsx
@@ -1,16 +1,35 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 export const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  const handleSignIn = async ({ email }: SignInForm) => {
+    console.log("I'm signing in with", email)
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+  }
+
   return (
     <>
       <Helmet title="Sign In" />
 
       <div className="p-8">
-        <div className="flex w-[350px] flex-col justify-center gap-6">
+        <div className="flex w-[350px] flex-col justify-center gap-16">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
               Acessar painel
@@ -20,13 +39,13 @@ export const SignIn = () => {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <fieldset className="space-y-2">
               <Label htmlFor="email">E-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </fieldset>
 
-            <Button className="w-full" type="submit">
+            <Button disabled={isSubmitting} className="w-full" type="submit">
               Acessar painel
             </Button>
           </form>
