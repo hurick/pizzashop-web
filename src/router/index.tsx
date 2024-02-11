cf6e5ea3e51768946597f25d1cb8750967a2487a
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-02-11 17:22:32 -0300

    chore(Pages): reorganize folder structure and filenames

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { LayoutApp } from '@/pages/_layouts/app'
 import { LayoutAuth } from '@/pages/_layouts/auth'
 import { Dashboard } from '@/pages/app/dashboard'
-import { Orders } from '@/pages/app/orders/orders'
+import { Orders } from '@/pages/app/orders'
 import { SignIn } from '@/pages/auth/sign-in'
 import { SignUp } from '@/pages/auth/sign-up'
 
