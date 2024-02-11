ff601c52ad7fe0d21ee7eabd330c4fce4f8f8a70
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-02-11 18:47:37 -0300

    refactor(Orders): minor table heading typo

--- src/pages/app/orders/index.tsx
+++ src/pages/app/orders/index.tsx
@@ -29,7 +29,7 @@ export const Orders = () => {
                 <TableRow>
                   <TableHead className="w-[64px]"></TableHead>
                   <TableHead className="w-[140px]">Identificador</TableHead>
-                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[180px]">Realizado</TableHead>
                   <TableHead className="w-[140px]">Status</TableHead>
                   <TableHead>Cliente</TableHead>
                   <TableHead className="w-[140px]">Total do pedido</TableHead>
