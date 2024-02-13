58553d35b8ab343298c1beb21195aa94d34f5358
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-02-13 17:03:47 -0300

    feat(Header): added API integrations

---
+++ src/api/managed-restaurant.ts
@@ -0,0 +1,18 @@
+import { api } from '@/lib/axios'
+
+interface GetManagedRestaurantResponse {
+  id: string
+  name: string
+  createdAt: Date | null
+  updatedAt: Date | null
+  managerId: string | null
+  description: string | null
+}
+
+export const getManagedRestaurant = async () => {
+  const response = await api.get<GetManagedRestaurantResponse>(
+    '/managed-restaurant',
+  )
+
+  return response.data
+}
