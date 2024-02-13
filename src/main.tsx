7c9a88ce3207f515775366b9ce254faa75d2b421
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-02-13 16:24:12 -0300

    feat: create project structure to start API calls

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import './styles/global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
@@ -7,6 +8,7 @@ import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 import { router } from './router'
 
 createRoot(document.getElementById('root')!).render(
@@ -15,7 +17,9 @@ createRoot(document.getElementById('root')!).render(
       <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   </StrictMode>,
