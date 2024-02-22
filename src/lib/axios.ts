1f8d6193e9dc49685abe38f99b3b2a3e63a00e6d
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-02-22 18:30:48 -0300

    feat(axios): create API delay interceptor

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,12 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  const DELAY_TIMER = 2000
+
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, DELAY_TIMER))
+    return config
+  })
+}
