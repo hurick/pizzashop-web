fa38fa9b0b092b7416bcd394540ac2d11b49526f
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-03-03 21:04:23 -0300

    feat(axios): added new delay function

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -8,7 +8,7 @@ export const api = axios.create({
 })
 
 if (env.VITE_ENABLE_API_DELAY) {
-  const DELAY_TIMER = 2000
+  const DELAY_TIMER = Math.round(Math.random() * 3000)
 
   api.interceptors.request.use(async (config) => {
     await new Promise((resolve) => setTimeout(resolve, DELAY_TIMER))
