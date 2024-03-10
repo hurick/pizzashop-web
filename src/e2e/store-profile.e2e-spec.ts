4092cf65c07d8a5e72ccbf391012ec8c1c7eb1f5
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-03-10 17:30:11 -0300

    tests(e2e/store-profile): add test suite

---
+++ src/e2e/store-profile.e2e-spec.ts
@@ -0,0 +1,49 @@
+import { expect, test } from '@playwright/test'
+
+test('successfully update profile', async ({ page }) => {
+  const toast = page.getByText('Perfil atualizado com sucesso')
+
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
+  await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+
+  await page.getByLabel('Nome').fill("Doe's Pizza")
+  await page.getByLabel('Descrição').fill('Nova descrição do restaurante')
+
+  await page.getByRole('button', { name: 'Salvar' }).click()
+
+  await page.waitForLoadState('networkidle')
+
+  expect(toast).toBeVisible()
+
+  await page.getByRole('button', { name: 'Close' }).click()
+  await page.waitForTimeout(100)
+
+  expect(page.getByRole('button', { name: "Doe's Pizza" })).toBeVisible()
+})
+
+test('unsuccessfully update profile with wrong credentials', async ({
+  page,
+}) => {
+  const toast = page.getByText('Ocorreu um erro ao atualizar o perfil')
+
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
+  await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+
+  await page.getByLabel('Nome').fill("Hurick's Pizza")
+  await page.getByLabel('Descrição').fill('Nova descrição do restaurante')
+
+  await page.getByRole('button', { name: 'Salvar' }).click()
+
+  await page.waitForLoadState('networkidle')
+
+  expect(toast).toBeVisible()
+
+  await page.getByRole('button', { name: 'Close' }).click()
+  await page.waitForTimeout(100)
+
+  expect(page.getByRole('button', { name: 'Pizza Shop' })).toBeVisible()
+})
