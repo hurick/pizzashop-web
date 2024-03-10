9fdc36dd55efc01d92a7ec132d5a63843f8854c9
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-03-10 16:53:31 -0300

    tests: add new testMatch config to standard filenames

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './src/e2e',
+  testMatch: /.*\.e2e-spec\.ts$/,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
