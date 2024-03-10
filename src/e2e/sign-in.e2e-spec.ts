eb100a574172d2390c77e37452e8e514597051d5
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-03-10 17:01:14 -0300

    tests(e2e/sign-in): fixed const declaration position

--- src/e2e/sign-in.e2e-spec.ts
+++ src/e2e/sign-in.e2e-spec.ts
@@ -1,25 +1,25 @@
 import { expect, test } from '@playwright/test'
 
 test('sign in successfully', async ({ page }) => {
+  const toast = page.getByText(
+    'Confira seu e-mail para continuar com sua autenticação',
+  )
+
   await page.goto('/sign-in', { waitUntil: 'networkidle' })
 
   await page.getByLabel('E-mail').fill('johndoe@example.com')
   await page.getByRole('button', { name: 'Acessar painel' }).click()
 
-  const toast = page.getByText(
-    'Confira seu e-mail para continuar com sua autenticação',
-  )
-
   expect(toast).toBeVisible()
 })
 
 test('sign in unsuccessfully with wrong credentials', async ({ page }) => {
+  const toast = page.getByText('Credenciais inválidas')
+
   await page.goto('/sign-in', { waitUntil: 'networkidle' })
 
   await page.getByLabel('E-mail').fill('wrong@example.com')
   await page.getByRole('button', { name: 'Acessar painel' }).click()
-
-  const toast = page.getByText('Credenciais inválidas')
 
   expect(toast).toBeVisible()
 })
