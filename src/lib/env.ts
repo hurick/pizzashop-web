574d2a659f7012afbf08dd24dfaf93f1ce46b633
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-03-10 12:53:01 -0300

    chore: add msw configuration setup and files

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,8 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  VITE_API_URL: z.string().url(),
+  MODE: z.enum(['dev', 'test', 'prod']),
+  VITE_API_URL: z.string(),
   VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
 })
 
--- src/main.tsx
+++ src/main.tsx
@@ -7,20 +7,23 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { enableMSW } from './api/mocks'
 import { ThemeProvider } from './components/theme/theme-provider'
 import { queryClient } from './lib/react-query'
 import { router } from './router'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <HelmetProvider>
-      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
-        <Helmet titleTemplate="%s | pizza.shop" />
-        <Toaster richColors />
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-        </QueryClientProvider>
-      </ThemeProvider>
-    </HelmetProvider>
-  </StrictMode>,
-)
+enableMSW().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <HelmetProvider>
+        <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+          <Helmet titleTemplate="%s | pizza.shop" />
+          <Toaster richColors />
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+          </QueryClientProvider>
+        </ThemeProvider>
+      </HelmetProvider>
+    </StrictMode>,
+  )
+})
