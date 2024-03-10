db0e5f0a20269a3d78f79092d6f2180115561dca
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-03-10 16:53:47 -0300

    tests(e2e/sign-in): add test suite

---
+++ src/e2e/sign-in.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { expect, test } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('E-mail').fill('johndoe@example.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText(
+    'Confira seu e-mail para continuar com sua autenticação',
+  )
+
+  expect(toast).toBeVisible()
+})
+
+test('sign in unsuccessfully with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('E-mail').fill('wrong@example.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Credenciais inválidas')
+
+  expect(toast).toBeVisible()
+})
+
+test('navigate to sign up restaurant', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Novo estabelecimento' }).click()
+
+  expect(page.url()).toContain('/sign-up')
+})
