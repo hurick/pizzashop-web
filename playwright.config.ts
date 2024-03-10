31677709d301e371f13910aae92151d0a547dbb8
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-03-10 16:33:06 -0300

    tests: add playwright setup and files

---
+++ playwright.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from '@playwright/test'
+
+export default defineConfig({
+  testDir: './src/e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: { baseURL: 'http://localhost:50789' },
+  webServer: {
+    command: 'pnpm dev:test',
+    url: 'http://localhost:50789',
+    reuseExistingServer: !process.env.CI,
+  },
+})
