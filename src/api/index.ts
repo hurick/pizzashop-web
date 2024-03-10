89fd92f882bcb2dc02e9fa000869e920abea9030
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-03-10 13:29:12 -0300

    test(get-monthly-revenue): add mock

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,6 +7,7 @@ import { signUpRestaurantMock } from './auth/mocks/sign-up-restaurant'
 import { getDailyOrderAmountMock } from './dashboard/cards/mocks/get-daily-order-amount'
 import { getMonthlyCanceledOrderAmountMock } from './dashboard/cards/mocks/get-monthly-canceled-order-amount'
 import { getMonthlyOrderAmountMock } from './dashboard/cards/mocks/get-monthly-order-amount'
+import { getMonthlyRevenueMock } from './dashboard/cards/mocks/get-monthly-revenue'
 
 export const worker = setupWorker(
   signInMock,
@@ -14,6 +15,7 @@ export const worker = setupWorker(
   getDailyOrderAmountMock,
   getMonthlyOrderAmountMock,
   getMonthlyCanceledOrderAmountMock,
+  getMonthlyRevenueMock,
 )
 
 export const enableMSW = async () => {
