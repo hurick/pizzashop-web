f578a3fbc1233e69900713a268fcddf99cb29820
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2024-03-10 13:15:31 -0300

    refactor(auth/mocks): file names

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,8 +2,8 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/lib/env'
 
-import { signInMock } from './auth/mocks/sign-in.mock'
-import { signUpRestaurantMock } from './auth/mocks/sign-up-restaurant-mock'
+import { signInMock } from './auth/mocks/sign-in'
+import { signUpRestaurantMock } from './auth/mocks/sign-up-restaurant'
 
 export const worker = setupWorker(signInMock, signUpRestaurantMock)
 
